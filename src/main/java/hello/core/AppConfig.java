5a0f18a67bc931bd294eb317c4ab84d60e03f8c2
unknown <alskadmlcraz1@gmail.com>
2024-05-11 13:30:32 +0900

    init project

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,24 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+/**
+ *	애플리케이션의 전체 동작 방식을 구성
+ *  - 구현 객체를 생성하고, 연결하는 책임을 가짐 
+ */
+public class AppConfig {
+
+	public MemberService memberSerivce() {
+		return new MemberServiceImpl(new MemoryMemberRepository());
+	}
+
+	public OrderService orderService() {
+		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+
+	}
+}
