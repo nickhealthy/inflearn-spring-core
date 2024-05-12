021356c2ed908727238d4045fd83bb3522f7578a
unknown <alskadmlcraz1@gmail.com>
2024-05-12 13:33:55 +0900

    init project

--- src/test/java/hello/core/MemberApp.java
+++ src/test/java/hello/core/MemberApp.java
@@ -2,13 +2,14 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemberService;
 
 /* 회원 도메인 - 회원 가입 main */
 public class MemberApp {
 
 	public static void main(String[] args) {
-		MemberServiceImpl memberService = new MemberServiceImpl();
+		AppConfig appConfig = new AppConfig();
+		MemberService memberService = appConfig.memberSerivce();
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
 
