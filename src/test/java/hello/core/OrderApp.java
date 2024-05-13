a26caa323e0b079337f2ea3c6694327a7df01214
nickhealthy <alskadmlcraz1@gmail.com>
2024-05-13 13:48:09 +0900

    init project

--- src/test/java/hello/core/OrderApp.java
+++ src/test/java/hello/core/OrderApp.java
@@ -14,12 +14,17 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
 	public static void main(String[] args) {
-		MemberService memberService = new MemberServiceImpl();
-		OrderService orderService = new OrderServiceImpl();
+		
+		// MemberService memberService = new MemberServiceImpl();
+		// OrderService orderService = new OrderServiceImpl();
+		
+		AppConfig appConfig = new AppConfig();
+		MemberService memberSerivce = appConfig.memberSerivce();
+		OrderService orderService = appConfig.orderService();
 		
 		long memberId = 1L;
 		Member member = new Member(memberId, "memberA", Grade.VIP);
-		memberService.join(member);
+		memberSerivce.join(member);
 		
 		Order order = orderService.createOrder(memberId, "itemA", 10000);
 		
