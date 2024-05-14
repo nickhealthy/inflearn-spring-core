615a6c58e86324da285368b27ecb4e29a3971870
nickhealthy <alskadmlcraz1@gmail.com>
2024-05-14 13:52:25 +0900

    init project

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,23 +1,34 @@
 package hello.core.member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import hello.core.AppConfig;
 
 public class MemberServiceTest {
 
-	MemberService memberService = new MemberServiceImpl();
-	
+	MemberService memberService;
+
+	@BeforeEach
+	public void beforeEach() {
+		AppConfig appConfig = new AppConfig();
+		memberService = appConfig.memberSerivce();
+	}
+
 	@Test
+	@DisplayName("회원 가입 및 조회")
 	void join() {
 		// given
 		Member member = new Member(1L, "memberA", Grade.VIP);
-		
+
 		// when
 		memberService.join(member);
 		Member findMember = memberService.findMember(1L);
-		
+
 		// then
 		Assertions.assertThat(member).isEqualTo(findMember);
 	}
-	
+
 }
