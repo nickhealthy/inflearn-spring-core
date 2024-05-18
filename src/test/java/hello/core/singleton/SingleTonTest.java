147374875efe0d6e3a3b731f30228b4a06bf67ce
nickhealthy <alskadmlcraz1@gmail.com>
2024-05-18 14:55:43 +0900

    ch5 - 싱글톤 패턴

--- src/test/java/hello/core/singleton/SingleTonTest.java
+++ src/test/java/hello/core/singleton/SingleTonTest.java
@@ -26,4 +26,25 @@ public class SingleTonTest {
 
 		Assertions.assertThat(memberService1).isNotSameAs(memberService2);
 	}
+
+	@Test
+	@DisplayName("싱글톤 패턴을 적용한 객체 사용")
+	public void singletonServiceTest() {
+
+		// private으로 생성자를 막아두었다. 컴파일 오류가 발생
+		// new SingletonService();
+
+		// 1. 조회: 호출할 때마다 같은 객체를 반환
+		SingletonService instance1 = SingletonService.getInstance();
+
+		// 2. 조회: 호출할 때마다 같은 객체를 반환
+		SingletonService instance2 = SingletonService.getInstance();
+
+		// 참조값이 같은 것을 확인
+		System.out.println(instance1);
+		System.out.println(instance2);
+
+		Assertions.assertThat(instance1).isSameAs(instance2);
+
+	}
 }
