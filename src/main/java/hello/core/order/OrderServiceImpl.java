d32a3a08c41427e04c7cea28ffe02e657fa28b35
nickhealthy <alskadmlcraz1@gmail.com>
2024-05-26 12:43:12 +0900

    ch7 - 생성자 주입을 선택해라!

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,16 +9,15 @@ import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
 	private MemberRepository memberRepository;
 	private DiscountPolicy discountPolicy;
 
-	/* [수정자 주입] 
-	 * - 선택, 변경 가능성이 있는 의존관계에 사용
-	 * - 생성자 주입 이후 Setter 주입이 발동하게 된다.
-	 * */
+	/*
+	 * [수정자 주입] - 선택, 변경 가능성이 있는 의존관계에 사용 - 생성자 주입 이후 Setter 주입이 발동하게 된다.
+	 */
 	@Autowired
 	public void setMemberRepository(MemberRepository memberRepository) {
 		System.out.println("MemberRepository = " + memberRepository);
@@ -31,15 +30,19 @@ public class OrderServiceImpl implements OrderService {
 		this.discountPolicy = discountPolicy;
 	}
 
-	/* [생성자 주입]
-	 * - 생성자 호출 시점에 딱 1번만 호출되는 것을 보장
-	 * - 주로 불변, 필수 의존관계에 사용 
-	 * */
-	/*public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+	public OrderServiceImpl() {
+		super();
+	}
+
+	/*
+	 * [생성자 주입] - 생성자 호출 시점에 딱 1번만 호출되는 것을 보장 - 주로 불변, 필수 의존관계에 사용
+	 */
+	public OrderServiceImpl(MemberRepository memberRepository,
+			DiscountPolicy discountPolicy) {
 		super();
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
-	}*/
+	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
